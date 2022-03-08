2231099dc22a9d3279caa0fd5b6695f2a07ed707
Mk <munikumar450@gmail.com>
2022-03-07 23:28:22 -0700

    changes wrt to ZooKeeper integration

--- src/main/java/com/muni/test/calledservice/CalledServiceApp.java
+++ src/main/java/com/muni/test/calledservice/CalledServiceApp.java
@@ -1,9 +1,10 @@
 package com.muni.test.calledservice;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
@@ -11,6 +12,8 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
 @EnableHystrix
 @EnableHystrixDashboard
 @EnableCircuitBreaker
+@RefreshScope
+@EnableDiscoveryClient
 public class CalledServiceApp {
 
 	public static void main(String[] args) {
